20c6b486cbd2838b2ce951a399aaca45a420bd15
yagmuronder <87575393+yagmuronder@users.noreply.github.com>
2023-03-22 00:59:05 +0100

    Project 2 - Color Change
    Ball changes color with collisions

---
+++ Project 2/Assets/Scripts/ChangeColorOnCollision.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColorOnCollision : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //void Start()
+    //{
+
+    //}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Color randomColor = GetRandomColor();
+        GetComponent<Renderer>().material.color = randomColor;
+    }
+
+    private Color GetRandomColor()
+    {
+        return new Color(
+            UnityEngine.Random.Range(0f, 1f),
+            UnityEngine.Random.Range(0f, 1f),
+            UnityEngine.Random.Range(0f, 1f));
+    }
+}
